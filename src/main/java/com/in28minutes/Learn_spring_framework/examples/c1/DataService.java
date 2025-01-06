3ac209b1fbe90967261f88243af9fcc23da032cd
kami-christella <kamichristella@gmail.com>
2025-01-06 23:11:00 +0200

    added codes

---
+++ src/main/java/com/in28minutes/Learn_spring_framework/examples/c1/DataService.java
@@ -0,0 +1,6 @@
+package com.in28minutes.Learn_spring_framework.examples.c1;
+
+public interface DataService {
+
+	int [] retrieveData();
+}
--- src/main/java/com/in28minutes/Learn_spring_framework/game/GamingAppLauncherApplication.java
+++ src/main/java/com/in28minutes/Learn_spring_framework/game/GamingAppLauncherApplication.java
@@ -1,23 +1,10 @@
-package com.in28minutes.Learn_spring_framework;
+package com.in28minutes.Learn_spring_framework.game;
 
-import com.in28minutes.Learn_spring_framework.game.MarioGame;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-
-import com.in28minutes.Learn_spring_framework.game.PacmanGame;
-
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-
-import com.in28minutes.Learn_spring_framework.game.GameRunner;
-import com.in28minutes.Learn_spring_framework.game.GamingConsole;
-import com.in28minutes.Learn_spring_framework.game.SuperContraGame;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import com.in28minutes.Learn_spring_framework.game.GameRunner;
-import com.in28minutes.Learn_spring_framework.game.GamingConsole;
-import com.in28minutes.Learn_spring_framework.game.PacmanGame;
 
 @Configuration
 @ComponentScan("com.in28minutes.Learn_spring_framework.game")
